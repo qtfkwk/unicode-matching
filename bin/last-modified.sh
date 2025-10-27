b7c6a5958c814b853b244bb94cd2e8d9d531ad83
qtfkwk <qtfkwk+unicode-matching@gmail.com>
2025-10-27 12:36:33 -0400

    0.6.2

--- bin/last-modified.sh
+++ bin/last-modified.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-curl -sI http://www.unicode.org/Public/UNIDATA/UnicodeData.txt |sed -n 's/last-modified: //Ip'
+curl -sI http://www.unicode.org/Public/UNIDATA/UnicodeData.txt |sed -n 's/last-modified: //Ip' |sed 's/\r//g'
 
--- t/lib.rs
+++ t/lib.rs
@@ -68,7 +68,7 @@ impl FindMatching for GString {
             return position;
         }
 
-        let mut stack: Vec<(usize, &String)> = vec![];
+        let mut stack: Vec<(usize, &Grapheme)> = vec![];
         for (i, g) in v.iter().enumerate() {
             if let Some(open) = opening.get(&g.as_str()) {
                 // g is a close
